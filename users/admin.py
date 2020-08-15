84654fd31faa007652539e69f36b5111963f8fec
PresentJay <presentj94@gmail.com>
2020-08-15 15:17:18 +0900

    feat: set useradmin fieldsets&displays, filter

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,23 @@ from . import models
 # Register your models here.
 
 
-# @admin.register(models.User)
 @admin.register(models.User)
 class Admin(UserAdmin):
 
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional", {"fields": ("avatar", "gender", "bio", "birthdate",),}),
+        (
+            "Additional",
+            {"fields": ("avatar", "gender", "bio", "birthdate", "login_method",),},
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter
+
+    list_display = (
+        "username",
+        "email",
+        "is_active",
+        "email_confirmed",
+        "email_secret",
+        "login_method",
     )
