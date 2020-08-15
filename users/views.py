bf9e10923f9cdb292aa6b158bc633bb1a265580c
PresentJay <presentj94@gmail.com>
2020-08-15 19:30:54 +0900

    feat: add userview of login, signup, and logout function

--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.views.generic import FormView
 
+from django.contrib.auth import authenticate, login, logout
+
+from . import forms, models, mixins
 
 # Create your views here.
 
 
-class LoginView:
-    pass
+class LoginView(mixins.LoggedOutOnlyView, FormView):
+
+    template_name = ""
+    form_class = None
+
+    def form_valid(self, form):
+        email = None
+        password = None
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("core:home")
