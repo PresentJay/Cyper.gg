ccf00068c67b483da0b75237914d0a0849f658a6
PresentJay <presentj94@gmail.com>
2020-08-15 15:14:13 +0900

    feat: update user models about choices of login methods(email, kakao, naver, facebook)

--- users/models.py
+++ users/models.py
@@ -10,9 +10,26 @@ class User(AbstractUser):
     GENDER_FEMALE = "female"
     GENDER_CHOICES = ((GENDER_MALE, "남자"), (GENDER_FEMALE, "여자"))
 
-    avatar = models.ImageField(blank=True)
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, blank=True
+    LOGIN_EMAIL = "email"
+    LOGIN_KAKAO = "kakao"
+    LOGIN_NAVER = "naver"
+    LOGIN_FACEBOOK = "facebook"
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_KAKAO, "Kakao"),
+        (LOGIN_NAVER, "Naver"),
+        (LOGIN_FACEBOOK, "FaceBook"),
     )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True)
+
+    email_confirmed = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=20, default="", blank=True)
+
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+
