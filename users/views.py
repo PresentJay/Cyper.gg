3676b5a8794342614153f04d88ca4ff256cfea40
PresentJay <presentj94@gmail.com>
2020-08-15 19:32:16 +0900

    chore: adjust view a little

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, redirect
+from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from django.contrib.auth import authenticate, login, logout
@@ -12,6 +13,7 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = ""
     form_class = None
+    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = None
@@ -25,3 +27,27 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     def get_success_url(self):
         return reverse("core:home")
+
+
+class SignUpView(mixins.LoggedOutOnlyView, FormView):
+    template_name = ""
+    form_class = None
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        # need form save
+        email = None
+        password = None
+        user = authenticate(self.request, username=email, password=password)
+        
+        if user is not None:
+            login(self.request, user)
+            
+        # need verify email
+        
+        return super().form_valid(form)
+    
+    
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
